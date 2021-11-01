6c3d709821cff70ee5e2d3f42263186563d57702
Thiago <thiagoluis_10@hotmail.com>
2021-11-01 18:09:39 -0300

    first commit

---
+++ src/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,28 @@
+package com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Gato gato = new Gato();
+		Livros livros = new Livros();
+		
+		System.out.println(gatos);
+		System.out.println(livros);
+	
+		
+		/*int a = 4;
+		int b = 5;
+		System.out.println("Hello World! " + (a+b));*/
+
+	}
+
+}
+
+class Livros {
+	private String nome;
+	private String npag;
+}
